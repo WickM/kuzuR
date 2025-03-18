204a262344d8053b683f29e91d7479f2d9ef4b90
Manuel <manuel.wick@gmail.com>
2025-03-18 23:49:07 +0100

    added try catch and informaton

--- R/kuzuConnect.R
+++ R/kuzuConnect.R
@@ -1,8 +1,17 @@
 
 
-kuzuConnect <- function(path=""){
+kuzuConnect <- function(path="",read_only=FALSE){
   reticulate::source_python("inst/kuzuConnect.py")
 
-  conn <- kuzuConnect_py(path = path)
-  return(conn)
+  tryCatch(
+    {
+      conn <- kuzuConnect_py(path = path, read_only = read_only)
+      cli::cli_alert_success("Kuzu connenction with Database:{path} established.")
+
+      return(conn)
+    },
+    error = function(e)
+      cli::cli_alert_warning("Kuzu connenction with Database:{path} already established. Restart Session for new connection")
+    )
 }
+
--- inst/kuzuConnect.py
+++ inst/kuzuConnect.py
@@ -1,6 +1,6 @@
 import kuzu
 
-def kuzuConnect_py (path):
-  db = kuzu.Database(path)
+def kuzuConnect_py (path,read_only):
+  db = kuzu.Database(path, read_only=read_only)
   conn = kuzu.Connection(db)
   return(conn)
